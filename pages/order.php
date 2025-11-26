ef6069d7092e8c8742910c2060c4f9ffbeb2e0df
enriohernanda <enriohernanda@gmail.com>
2025-11-26 14:38:49 +0700

    add order

--- pages/order.php
+++ pages/order.php
@@ -1,20 +1,13 @@
 <?php
-$query = mysqli_query($koneksi, "SELECT * FROM orders ORDER BY id DESC");
+$query = mysqli_query($config, "SELECT c.name, `to`.* FROM trans_orders `to` LEFT JOIN customers c ON c.id = `to`.customer_id ORDER BY id DESC");
 $rows = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
 if (isset($_GET['delete'])) {
     $id = $_GET['delete'];
 
-    $s_photo = mysqli_query($koneksi, "SELECT product_photo FROM products WHERE id = $id");
-    $row = mysqli_fetch_assoc($s_photo);
-    $filePath = $row['product_photo'];
-    if (file_exists($filePath)) {
-        unlink($filePath);
-    }
-
-    $delete = mysqli_query($koneksi, "DELETE FROM products WHERE id = $id");
+    $delete = mysqli_query($config, "DELETE FROM trans_orders WHERE id = $id");
     if ($delete) {
-        header("location:?page=product");
+        header("location:?page=order");
     }
 }
 
@@ -24,23 +17,22 @@ if (isset($_GET['delete'])) {
     <div class="row">
         <div class="col-sm-12">
             <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Data Products</h3>
-                </div>
                 <div class="card-body">
+                    <h3 class="card-title">Data Order</h3>
                     <div class="d-flex justify-content-end mb-3 mt-2">
-                        <a class="btn btn-primary" href="pos/add-pos.php"><i class="bi bi-plus-circle"></i> Add
-                            POS</a>
+                        <a class="btn btn-primary" href="pos/add-order.php"><i class="bi bi-plus-circle"></i> Add
+                            Order</a>
                     </div>
                     <table class="table table-bordered">
                         <tr class="text-center">
                             <th>No</th>
                             <th>Order Code</th>
-                            <th>Order Date</th>
-                            <th>Order Amount</th>
+                            <th>Order End Date</th>
+                            <th>Order Total</th>
+                            <th>Order Tax</th>
+                            <th>Order Pay</th>
                             <th>Order Change</th>
                             <th>Status</th>
-                            <th>Subtotal</th>
                             <th>Actions</th>
                         </tr>
                         <?php foreach ($rows as $key => $v) {
@@ -48,11 +40,12 @@ if (isset($_GET['delete'])) {
                             <tr>
                                 <td class="text-center"><?php echo $key + 1 ?></td>
                                 <td><?php echo $v['order_code'] ?></td>
-                                <td><?php echo $v['order_date'] ?></td>
-                                <td><?php echo $v['order_amount'] ?></td>
+                                <td><?php echo $v['order_end_date'] ?></td>
+                                <td><?php echo $v['order_total'] ?></td>
+                                <td><?php echo $v['order_tax'] ?></td>
+                                <td><?php echo $v['order_pay'] ?></td>
                                 <td><?php echo $v['order_change'] ?></td>
                                 <td class="text-center"><?php echo $v['order_status'] ?></td>
-                                <td class="text-center"><?php echo $v['order_subtotal'] ?></td>
                                 <td class="text-center">
                                     <a href="?page=tambah-product&edit=<?php echo $v['id'] ?>"
                                         class="btn btn-success btn-sm"><i class="bi bi-pencil"></i></a>
