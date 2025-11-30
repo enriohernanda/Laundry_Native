dce9e5efba5ef57bc322ff10bd60193e708307fd
enriohernanda <enriohernanda@gmail.com>
2025-11-30 11:56:04 +0700

    menambahkan id saat redirect dari add order ke print

--- assets/js/enrio.js
+++ assets/js/enrio.js
@@ -1,43 +1,43 @@
 function selectCustomers() {
-      const select = document.getElementById('customer_id');
-      const phone = select.options[select.selectedIndex].getAttribute('data-phone');
-      document.getElementById('phone').value = phone || "";
-  }
+  const select = document.getElementById('customer_id');
+  const phone = select.options[select.selectedIndex].getAttribute('data-phone');
+  document.getElementById('phone').value = phone || '';
+}
 
 function openModal(service) {
-    document.getElementById('modal_id').value = service.id;
-    document.getElementById('modal_name').value = service.name;
-    document.getElementById('modal_price').value = service.price;
-    document.getElementById('modal_qty').value = service.qty;
+  document.getElementById('modal_id').value = service.id;
+  document.getElementById('modal_name').value = service.name;
+  document.getElementById('modal_price').value = service.price;
+  document.getElementById('modal_qty').value = service.qty;
 
-    new bootstrap.Modal('#exampleModal').show();
+  new bootstrap.Modal('#exampleModal').show();
 }
 
 let cart = [];
 
 function addToCart() {
-    const id = document.getElementById('modal_id').value;
-    const name = document.getElementById('modal_name').value;
-    const price = parseFloat(document.getElementById('modal_price').value);
-    const qty = parseFloat(document.getElementById('modal_qty').value);
-    
-    const existing = cart.find((item) => item.id == id);
-    if (existing) {
-        existing.quantity += qty;
-    } else {
-        cart.push({
-            id,
-            name,
-            price,
-            qty
-        });
-    }
-    renderCart();
+  const id = document.getElementById('modal_id').value;
+  const name = document.getElementById('modal_name').value;
+  const price = parseFloat(document.getElementById('modal_price').value);
+  const qty = parseFloat(document.getElementById('modal_qty').value);
+
+  const existing = cart.find((item) => item.id == id);
+  if (existing) {
+    existing.quantity += qty;
+  } else {
+    cart.push({
+      id,
+      name,
+      price,
+      qty,
+    });
+  }
+  renderCart();
 }
 
 function renderCart() {
-  const cartContainer = document.querySelector("#cartItems");
-  cartContainer.innerHTML = "";
+  const cartContainer = document.querySelector('#cartItems');
+  cartContainer.innerHTML = '';
 
   if (cart.length === 0) {
     cartContainer.innerHTML = `<div class="cart-items" id="cartItems">
@@ -46,12 +46,12 @@ function renderCart() {
             <p>Empty Cart</p>
           </div>
       </div>`;
-      updateTotal();
-      // return;
+    updateTotal();
+    // return;
   }
   cart.forEach((item, index) => {
-    const div = document.createElement("div");
-    div.className ="cart-item d-flex justify-content-between align-items-center mb-2";
+    const div = document.createElement('div');
+    div.className = 'cart-item d-flex justify-content-between align-items-center mb-2';
     div.innerHTML = `
         <div>
           <strong>${item.name}</strong>
@@ -66,7 +66,7 @@ function renderCart() {
     cartContainer.appendChild(div);
   });
   updateTotal();
-  calculateChange(); 
+  calculateChange();
 }
 
 // menghapus item dari cart
@@ -83,7 +83,7 @@ function changeQty(id, x) {
   }
   item.qty += x;
   if (item.qty <= 0) {
-    alert("Minimum must be 1 product!");
+    alert('Minimum must be 1 product!');
     item.qty += 1;
     // cart = filter((p) => p.id != id);
   }
@@ -91,81 +91,78 @@ function changeQty(id, x) {
 }
 
 function updateTotal() {
-    const subtotal = cart.reduce((sum, item) => sum + parseFloat(item.price) * parseFloat(item.qty), 0);
-    // const subtotal = price * qty;
-    const taxValue = document.querySelector('.tax').value;
-    let tax = parseInt(taxValue) / 100;
-    tax = subtotal * parseFloat(tax);
-    const total = tax + subtotal;
+  const subtotal = cart.reduce((sum, item) => sum + parseFloat(item.price) * parseFloat(item.qty), 0);
+  // const subtotal = price * qty;
+  const taxValue = document.querySelector('.tax').value;
+  let tax = parseInt(taxValue) / 100;
+  tax = subtotal * parseFloat(tax);
+  const total = tax + subtotal;
 
-    document.getElementById("subtotal").textContent = `Rp. ${subtotal.toLocaleString()}`;
-    document.getElementById("tax").textContent = `Rp. ${tax.toLocaleString()}`;
-    document.getElementById("total").textContent = `Rp. ${total.toLocaleString()}`;
+  document.getElementById('subtotal').textContent = `Rp. ${subtotal.toLocaleString()}`;
+  document.getElementById('tax').textContent = `Rp. ${tax.toLocaleString()}`;
+  document.getElementById('total').textContent = `Rp. ${total.toLocaleString()}`;
 
-    document.getElementById("subtotal_value").value = subtotal;
-    document.getElementById("tax_value").value = tax;
-    document.getElementById("total_value").value = total;
+  document.getElementById('subtotal_value').value = subtotal;
+  document.getElementById('tax_value').value = tax;
+  document.getElementById('total_value').value = total;
 }
 
-document.getElementById("clearCart").addEventListener("click", 
-    function() {
-        cart = [];
-        renderCart();
+document.getElementById('clearCart').addEventListener('click', function () {
+  cart = [];
+  renderCart();
 });
 
 async function processPayment() {
-    if (cart.length === 0){
-        alert("Cart still empty!");
-        return;
+  if (cart.length === 0) {
+    alert('Cart still empty!');
+    return;
+  }
+
+  const order_code = document.querySelector('.orderNumber').textContent.trim();
+  const subtotal = document.querySelector('#subtotal_value').value.trim();
+  const tax = document.querySelector('#tax_value').value.trim();
+  const grandTotal = document.querySelector('#total_value').value.trim();
+  const customer_id = parseInt(document.getElementById('customer_id').value);
+  const end_date = document.getElementById('end_date').value;
+
+  const pay = parseFloat(document.getElementById('pay').value);
+  const change = parseFloat(document.getElementById('change').value);
+
+  try {
+    const res = await fetch('add-order.php?payment', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        cart,
+        order_code,
+        subtotal,
+        tax,
+        grandTotal,
+        customer_id,
+        end_date,
+        pay,
+        change,
+      }),
+    });
+
+    const data = await res.json();
+    if (data.status == 'success') {
+      alert('Transaction Successfully!');
+      window.location.href = 'print.php?id=' + data.order_id;
+    } else {
+      alert('Transaction Failed: ' + data.message);
     }
-
-    const order_code = document.querySelector('.orderNumber').textContent.trim();
-    const subtotal = document.querySelector('#subtotal_value').value.trim();
-    const tax = document.querySelector('#tax_value').value.trim();
-    const grandTotal = document.querySelector('#total_value').value.trim();
-    const customer_id = parseInt(document.getElementById('customer_id').value);
-    const end_date = document.getElementById('end_date').value;
-
-    const pay = parseFloat(document.getElementById('pay').value);
-    const change = parseFloat(document.getElementById('change').value);
-
-    try {
-        const res = await fetch("add-order.php?payment", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ 
-                cart, 
-                order_code, 
-                subtotal, 
-                tax, 
-                grandTotal, 
-                customer_id, 
-                end_date,
-                pay,
-                change
-            }),
-        });
-
-        const data = await res.json();
-        if (data.status == "success"){
-            alert("Transaction Successfully!");
-            window.location.href = "print.php";
-        } else {
-            alert("Transaction Failed: " + data.message);
-        }
-
-    } catch (error) {
-        alert("Upss Transaction failed!");
-        console.log("error", error);
-    }
+  } catch (error) {
+    alert('Upss Transaction failed!');
+    console.log('error', error);
+  }
 }
 
-
-function calculateChange(){
+function calculateChange() {
   const total = document.getElementById('total_value').value;
   const pay = parseFloat(document.getElementById('pay').value);
 
   const change = pay - total;
   if (change < 0) change = 0;
-  document.getElementById("change").value = change;
+  document.getElementById('change').value = change;
 }
