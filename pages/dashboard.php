372ce2580678abf73fdd31bf131fb620b8332760
enriohernanda <enriohernanda@gmail.com>
2025-12-01 09:37:50 +0700

    update dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,38 +1,96 @@
+<?php
+// Total order
+$total_order = mysqli_fetch_assoc(
+    mysqli_query($config, "SELECT COUNT(*) AS total FROM trans_orders")
+)['total'];
+
+// Sedang diproses (Belum diambil)
+$proses = mysqli_fetch_assoc(
+    mysqli_query($config, "SELECT COUNT(*) AS total FROM trans_orders WHERE order_status = 0")
+)['total'];
+
+// Selesai (Sudah diambil)
+$pickUp = mysqli_fetch_assoc(
+    mysqli_query($config, "SELECT COUNT(*) AS total FROM trans_orders WHERE order_status = 1")
+)['total'];
+?>
+
+
 <div class="pagetitle">
-    <h1>Blank Page</h1>
-    <nav>
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-            <li class="breadcrumb-item">Pages</li>
-            <li class="breadcrumb-item active">Blank</li>
-        </ol>
-    </nav>
-</div><!-- End Page Title -->
+    <h1>Dashboard Laundry</h1>
+</div>
+<!-- End Page Title -->
 
-<section class="section">
+<section class="section dashboard">
+
     <div class="row">
-        <div class="col-lg-6">
 
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Example Card</h5>
-                    <p>This is an examle page with no contrnt. You can use it as a starter for your custom
-                        pages.</p>
-                </div>
+        <!-- Left side columns -->
+        <div class="col-lg-12">
+            <div class="row">
+
+                <!-- Pesanan Masuk -->
+                <div class="col-xxl-3 col-md-6">
+                    <div class="card info-card sales-card">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Order Total</span></h5>
+
+                            <div class="d-flex align-items-center">
+                                <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                    <i class="bi bi-bag-plus"></i>
+                                </div>
+                                <div class="ps-3">
+                                    <h6><?= $total_order ?></h6>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div><!-- End Pesanan Masuk -->
+
+                <!-- Proses -->
+                <div class="col-xxl-3 col-md-6">
+                    <div class="card info-card revenue-card">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Processing</span></h5>
+
+                            <div class="d-flex align-items-center">
+                                <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                    <i class="bi bi-repeat"></i>
+                                </div>
+                                <div class="ps-3">
+                                    <h6><?= $proses ?></h6>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div><!-- End Proses -->
+
+                <!-- pickUp -->
+                <div class="col-xxl-3 col-xl-3 col-md-6">
+                    <div class="card info-card customers-card">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Picked Up</h5>
+
+                            <div class="d-flex align-items-center">
+                                <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                    <i class="bi bi-check-circle"></i>
+                                </div>
+                                <div class="ps-3">
+                                    <h6><?= $pickUp ?></h6>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div><!-- End Selesai -->
+
             </div>
+        </div><!-- End Left side -->
 
-        </div>
-
-        <div class="col-lg-6">
-
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Example Card</h5>
-                    <p>This is an examle page with no contrnt. You can use it as a starter for your custom
-                        pages.</p>
-                </div>
-            </div>
-
-        </div>
     </div>
 </section>
